697c6fb267a83fa1d13d22395405d63206d64044
RICHARD <RICHARD@DESKTOP-8AK5O5L>
2017-01-21 14:16:29 +0100

    test interface

--- PortHub/src/main/java/PortHub/WebBrowser/BrowserSample.java
+++ PortHub/src/main/java/PortHub/WebBrowser/BrowserSample.java
@@ -21,6 +21,27 @@ public class BrowserSample extends Application
 	browser.loadURL("http://www.facebook.com"); 
 	
 	}
+	
+	/*public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			GridPane accueil = (GridPane) FXMLLoader.load(Main.class.getResource("../Interface/login.fxml"));
+			Scene scene = new Scene(accueil);
+			scene.setRoot(accueil);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("CTM");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}*/
 	 
 	public static void main(String[] args) {
 		launch(args);
--- PortHub/src/main/java/PortHub/WebBrowser/Controller/Controller_page_map.java
+++ PortHub/src/main/java/PortHub/WebBrowser/Controller/Controller_page_map.java
@@ -3,7 +3,9 @@ package PortHub.WebBrowser.Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller_page_map implements Initializable {
 	
