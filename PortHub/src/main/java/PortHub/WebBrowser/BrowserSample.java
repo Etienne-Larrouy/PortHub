5acf7789280bbc25411dd6bf0c7951dbe6c97629
RICHARD <RICHARD@DESKTOP-8AK5O5L>
2017-01-21 13:34:06 +0100

    web browser

--- PortHub/src/main/java/PortHub/WebBrowser/BrowserSample.java
+++ PortHub/src/main/java/PortHub/WebBrowser/BrowserSample.java
@@ -1,20 +1,31 @@
 package PortHub.WebBrowser;
 
-import com.teamdev.jxbrowser.Browser;
-import com.teamdev.jxbrowser.BrowserFactory;
-import com.teamdev.jxbrowser.BrowserType;
+import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
+ 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class BrowserSample 
+public class BrowserSample extends Application
 {    
-	public static void main(String[] args) 
-	{        Browser browser = BrowserFactory.createBrowser(BrowserType.Mozilla);         
-			 JFrame frame = new JFrame();
-			 frame.add(browser.getComponent(), BorderLayout.CENTER);
-			 frame.setSize(700, 500);
-			 frame.setLocationRelativeTo(null);
-			 frame.setVisible(true);
-			 browser.navigate("http://www.google.com");    
+	public void start(final Stage primaryStage) {
+	Browser browser = new Browser();
+	BrowserView view = new BrowserView(browser);
+	 
+	Scene scene = new Scene(new BorderPane(view), 700, 500);
+	primaryStage.setScene(scene);
+	primaryStage.show();
+	 
+	browser.loadURL("http://www.facebook.com");
+	}
+	 
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
