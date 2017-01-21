4570bc9ba75ab86bc3bd122430d4d3807b684fb3
jojo- <jojo-@DESKTOP-P7BF7K8>
2017-01-21 13:57:58 +0100

    add controller

---
+++ PortHub/src/main/java/PortHub/WebBrowser/Controller/Controller_page_map.java
@@ -0,0 +1,20 @@
+package PortHub.WebBrowser.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Controller_page_map implements Initializable {
+	
+	@FXML
+	private AnchorPane map;
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
