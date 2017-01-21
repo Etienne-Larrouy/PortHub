555e535d5c032d1288543bea70f0ad8dec652936
clement-subtil/Clement <clement.subtil72@gmail.com>
2017-01-21 11:55:21 +0100

    Projet MAVEN sa mère

---
+++ PortHub/src/main/java/PortHub/javax/json_api/App.java
@@ -0,0 +1,17 @@
+package PortHub.javax.json_api;
+
+import org.json.simple.JSONObject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	JSONObject json;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
