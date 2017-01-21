5d394cd5ab0b77c7a6e66c7387a5a3cb42924e44
jojo- <jojo-@DESKTOP-P7BF7K8>
2017-01-21 20:41:14 +0100

    c'est bon

---
+++ PortHub/src/main/java/Model/Card_Malus.java
@@ -0,0 +1,30 @@
+package Model;
+
+
+public class Card_Malus extends Card{
+	
+	private String malus;
+
+	public Card_Malus(String name,String malus) {
+		super(name);
+		// TODO Auto-generated constructor stub
+		this.malus = malus;
+	}
+	
+	
+	public void Action(){
+		if(malus.equals("accidente")){
+			System.out.println("accident");
+		}
+		if(malus.equals("snow") || malus.equals("traffic") || malus.equals("manif")){
+			System.out.println("snow traffic or manif");
+		}
+		if(malus.equals("fast")){
+			System.out.println("fast");
+		}
+		
+		
+		
+	}
+
+}
