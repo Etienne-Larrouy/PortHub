d5bf46602675dd1bbcd40a401a9d22f81c96d5c3
RICHARD <RICHARD@DESKTOP-8AK5O5L>
2017-01-21 15:04:54 +0100

    test

--- PortHub/src/main/java/PortHub/WebBrowser/BrowserSample.java
+++ PortHub/src/main/java/PortHub/WebBrowser/BrowserSample.java
@@ -2,55 +2,29 @@ package PortHub.WebBrowser;
 
 import java.io.IOException;
 
-import com.teamdev.jxbrowser.chromium.Browser;
-import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class BrowserSample extends Application
 {    
+	@Override
 	public void start(final Stage primaryStage) throws IOException {
-	/*Browser browser = new Browser();
-	BrowserView view = new BrowserView(browser);
-	 
-	Scene scene = new Scene(new BorderPane(view), 700, 500);
-	primaryStage.setScene(scene);
-	primaryStage.show();
-	 
-	browser.loadURL("http://www.facebook.com"); 
-	*/
-		FXMLLoader root =  new FXMLLoader(getClass().getResource("../View/page_map.fxml"));
-        Browser browser = new Browser();
-        BrowserView view = new BrowserView(browser);
-
-        Scene scene = new Scene(new BorderPane(view), 700, 500);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        browser.loadURL("http://www.google.com");
+		SplitPane page =  (SplitPane) FXMLLoader.load(BrowserSample.class.getResource("/View/page_map.fxml"));
+		Scene scene = new Scene(page);
+		primaryStage.setScene(scene);
+		scene.setRoot(page);
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
 	}
 	 
 	public static void main(String[] args) {
 		launch(args);
 	}
-	/*Stage stage = null;
-		Parent root = null;
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Task.fxml"));
-		
-	
-		ControllerTask controller = new ControllerTask(s.getObservableListTasks().get(Integer.parseInt(TaskPreview_id.getText())));
-	        // Set it in the FXMLLoader
-		loader.setController(controller);
 
-		stage = (Stage) ((GridPane)event.getSource()).getScene().getWindow();
-		
-		root = (Parent)loader.load();
-		Scene scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();*/
 }
