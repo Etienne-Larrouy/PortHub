08128c21d099eccb78c248a79a371cb07347eb15
Etienne Larrouy <checkarms@gmail.com>
2017-01-21 12:14:10 +0100

    Dependencies

---
+++ PortHub/src/main/java/PortHub/testMappyAPI/AjaxController.java
@@ -0,0 +1,21 @@
+package PortHub.testMappyAPI;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AjaxController {
+
+	@ResponseBody
+	@RequestMapping(value = "/search/api/getSearchResult")
+	public AjaxResponseBody getSearchResultViaAjax(@RequestBody SearchCriteria search) {
+
+		AjaxResponseBody result = new AjaxResponseBody();
+		//logic
+		return result;
+
+	}
+
+}
