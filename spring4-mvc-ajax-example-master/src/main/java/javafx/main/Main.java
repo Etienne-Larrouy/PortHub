9e87b729130f0db77d088a033580bb98c2abb078
jojo- <jojo-@DESKTOP-P7BF7K8>
2017-01-22 03:59:56 +0100

    carte

--- spring4-mvc-ajax-example-master/src/main/java/javafx/main/Main.java
+++ spring4-mvc-ajax-example-master/src/main/java/javafx/main/Main.java
@@ -3,6 +3,12 @@ package javafx.main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -19,9 +25,13 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			scene.setRoot(page);
 			primaryStage.setScene(scene);
+			
+			BackgroundImage myBI= new BackgroundImage(new Image("/fond.png",800,800,false,true),
+			        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+			          BackgroundSize.DEFAULT);
+			page.setBackground(new Background(myBI));
 			primaryStage.setResizable(false);
 			primaryStage.show();
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
